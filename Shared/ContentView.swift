41d5fae711479ed0a8836e48a8de035d9621b410
68018820+AlexeySobolevskiy@users.noreply.github.com <68018820+AlexeySobolevskiy@users.noreply.github.com>
2022-11-21 14:38:43 +0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Алексей Соболевский on 21.11.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Thread_safe_SingletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thread_safe_SingletonApp.swift
+//  Shared
+//
+//  Created by Алексей Соболевский on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Thread_safe_SingletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
